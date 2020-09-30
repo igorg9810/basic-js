02d9bae2625e9399a20131108fc7708b693e88a8
igorg9810 <turgon98@gmail.com>
2020-09-30 13:31:23 +0300

    init: complete first 4 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)){
+    return false;
+  }
+  let dream_name = []
+  for (let name of members){
+    if (typeof name != "string"){
+      continue;
+    }
+    else{
+      dream_name.push(name.trim()[0].toUpperCase());
+    }
+  }
+  return dream_name.sort().join("");
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (str == undefined){
+    return false;
+  }
+  if (options.separator == undefined){
+    options.separator = "+";
+  }
+  if (options.additionSeparator == undefined){
+    options.additionSeparator = "|";
+  }
+  if (options.repeatTimes == undefined){
+    options.repeatTimes = 0;
+  }
+  if (options.additionRepeatTimes == undefined){
+    options.additionRepeatTimes = 0;
+  }
+  options.addition = String(options.addition);
+  str = String(str);
+  let repeat_addition = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++){
+    repeat_addition.push(options.addition)
+  }
+  repeat_addition = repeat_addition.join(options.additionSeparator);
+  let repeat_str = [];
+  for (i = 0; i < options.repeatTimes; i++){
+    repeat_str.push(str + repeat_addition)
+  }
+  return repeat_str.join(options.separator);
 };
   
