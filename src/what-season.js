d6cb4f23af1514a5059948c95d0043be897e8223
igorg9810 <turgon98@gmail.com>
2020-10-05 19:05:31 +0300

    fix: finish what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  if (date == undefined){
+    return "Unable to determine the time of year!";
+  }
+  if (!(Object.prototype.toString.call(date) === '[object Date]')){
+    throw Error("Argument is invalid!")
+  }
   let month = date.getMonth();
   if (month < 2 || month == 11) return 'winter';
-  if (month < 6 && month >= 2) return 'spring';
-  if (month < 9 && month >= 6) return 'summer';
+  if (month < 5 && month >= 2) return 'spring';
+  if (month < 8 && month >= 5) return 'summer';
   else return 'autumn';
 };
