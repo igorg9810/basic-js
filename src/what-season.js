c71fe105f45f2878d2056c6370b2fdb6bfa2ad9f
igorg9810 <turgon98@gmail.com>
2020-10-05 12:47:40 +0300

    fix: solve what season task without testing

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let month = date.getMonth();
+  if (month < 2 || month == 11) return 'winter';
+  if (month < 6 && month >= 2) return 'spring';
+  if (month < 9 && month >= 6) return 'summer';
+  else return 'autumn';
 };
