da5f8c56e3bc1e0ea041682868696e60c71dd379
igorg9810 <turgon98@gmail.com>
2020-10-07 17:33:23 +0300

    fix: solved vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    constructor(mode) {
+        if (mode == undefined) {
+            this.mode = true;
+        } else {
+            this.mode = mode;
+        }
+        this.a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    encrypt(message, key) {
+        if (message == undefined || key == undefined) {
+            throw Error();
+        }
+        let maxlength = Math.max(message.length, key.length);
+        let r = '';
+        let offset = 0;
+        for (let i = 0; i < maxlength; i++) {
+            let s = message[((i >= message.length) ? i % message.length : i)].toUpperCase()
+            let mi = this.a.indexOf(s);
+            if (mi == -1) {
+                r += s;
+                offset++;
+                continue;
+            }
+            let ki_s = key[((i - offset >= key.length) ? (i - offset) % key.length : i - offset)];
+            //console.log(ki_s)
+            let ki = this.a.indexOf(ki_s.toUpperCase());
+            //console.log(ki)
+            let c = this.a[(((this.a.length + (mi + ki)) % this.a.length))]
+            r += c;
+        }
+        if (this.mode) {
+            return r.toUpperCase();
+        } else {
+            return r.toUpperCase().split("").reverse().join("");
+        }
+    }
+    decrypt(encryptedMessage, key) {
+        if (encryptedMessage == undefined || key == undefined) {
+            throw Error();
+        }
+        let maxlength = Math.max(encryptedMessage.length, key.length);
+        let r = '';
+        let offset = 0;
+        for (let i = 0; i < maxlength; i++) {
+            let s = encryptedMessage[((i >= encryptedMessage.length) ? i % encryptedMessage.length : i)].toUpperCase()
+            let mi = this.a.indexOf(s);
+            if (mi == -1) {
+                r += s;
+                offset++;
+                continue;
+            }
+            let ki_s = key[((i - offset >= key.length) ? (i - offset) % key.length : i - offset)];
+            let ki = -this.a.indexOf(ki_s.toUpperCase());
+            let c = this.a[(((this.a.length + (mi + ki)) % this.a.length))]
+            r += c;
+        }
+        if (this.mode) {
+            return r.toUpperCase();
+        } else {
+            return r.toUpperCase().split("").reverse().join("");
+        }
+    }
 }
 
 module.exports = VigenereCipheringMachine;
